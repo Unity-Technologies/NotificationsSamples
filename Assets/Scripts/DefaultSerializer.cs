f50a2c9f354d6e3f830d79636e8b3e7fb399b291
Aurimas Cernius <aurimasc@unity3d.com>
2022-05-03 18:57:37 +0300

    Flush when finished serializing

--- Assets/Scripts/DefaultSerializer.cs
+++ Assets/Scripts/DefaultSerializer.cs
@@ -77,6 +77,7 @@ namespace NotificationSamples
                             writer.Write(notification.DeliveryTime.Value.Ticks);
                         }
 
+                        writer.Flush();
                     }
                 }
             }
