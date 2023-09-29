c80644f666894ab5f7605cb3444ebfb50c5d390e
Aurimas Cernius <aurimasc@unity3d.com>
2023-09-29 16:13:49 +0300

    Doc comments for GameNotification

--- Assets/Scripts/GameNotification.cs
+++ Assets/Scripts/GameNotification.cs
@@ -2,6 +2,9 @@ using Unity.Notifications;
 
 namespace NotificationSamples
 {
+    /// <summary>
+    /// Represents a notification that will be delivered for this application.
+    /// </summary>
     public class GameNotification : IGameNotification
     {
         private Notification internalNotification;
@@ -11,23 +14,40 @@ namespace NotificationSamples
         /// </summary>
         public Notification InternalNotification => internalNotification;
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets a unique identifier for this notification.
+        /// </summary>
         /// <remarks>
-        /// Internally stored as a string. Gets parsed to an integer when retrieving.
+        /// <para>
+        /// If null, will be generated automatically once the notification is delivered, and then
+        /// can be retrieved afterwards.
+        /// </para>
+        /// <para>On some platforms, this might be converted to a string identifier internally.</para>
         /// </remarks>
-        /// <value>The identifier as an integer, or null if the identifier couldn't be parsed as a number.</value>
+        /// <value>A unique integer identifier for this notification, or null (on some platforms) if not explicitly set.</value>
         public int? Id { get => internalNotification.Identifier; set => internalNotification.Identifier = value; }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets the notification's title.
+        /// </summary>
+        /// <value>The title message for the notification.</value>
         public string Title { get => internalNotification.Title; set => internalNotification.Title = value; }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets the body text of the notification.
+        /// </summary>
+        /// <value>The body message for the notification.</value>
         public string Body { get => internalNotification.Text; set => internalNotification.Text = value; }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets optional arbitrary data for the notification.
+        /// </summary>
         public string Data { get => internalNotification.Data; set => internalNotification.Data = value; }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets the badge number for this notification. No badge number will be shown if null.
+        /// </summary>
+        /// <value>The number displayed on the app badge.</value>
         public int BadgeNumber
         {
             get => internalNotification.Badge;
